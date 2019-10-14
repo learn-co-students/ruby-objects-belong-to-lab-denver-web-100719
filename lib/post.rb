4157afa492ca17e4505c8b634fa752c543e5fba6
Amy Nguyen <amy.nguyen730@gmail.com>
2019-10-14 13:15:59 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+class Post
+    attr_reader :title, 
+                :author
+    def initialize(title author)
+        @title = title
+        @author = author
+    end
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+    attr_reader :title, 
+                :artist
+                
+    def intialize(title, artist)
+        @title = title
+        @artist = artist
+    end
+end
